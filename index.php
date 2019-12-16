104a4addfa80e47647a5680502a6b94419c5dd0d
leweke <male233@001NTB1066.ad.aconso.com>
2019-12-16 10:08:54 +0100

    refactoring
    - alternate php control structure syntax

--- index.php
+++ index.php
@@ -12,7 +12,20 @@
         <?php if(authenticate()): ?>
             <p>Beep</p>
         <?php else: ?>
-            <p>Boop</p>
+            <form type="GET" action="/index.php">
+                <div id="login-card" class="card mt-2">
+                    <div class="card-header">
+                        <h2>Login</h2>
+                    </div>
+                    <div class="card-body">
+                        <input class="form-control mb-2" type="text" name="username" placeholder="Username">
+                        <input class="form-control" type="password" name="password" placeholder="Password">
+                    </div>
+                    <div class="card-footer">
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                    </div>
+                </div>
+            </form>
         <?php endif ?>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
