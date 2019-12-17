50ebe719ccfdba6717e3dc072932d038753ec5f8
leweke <male233@001NTB1066.ad.aconso.com>
2019-12-17 13:23:33 +0100

    more templating

--- src/content/app.php
+++ src/content/app.php
@@ -1,4 +1,5 @@
 <div class="card">
+    <!-- MAIN MENU -->
     <ul class="card-header nav nav-tabs" role="tablist">
         <li class="nav-item">
             <a class="nav-link active" href="#workouts" data-toggle="tab">Workouts</a>
@@ -7,13 +8,17 @@
             <a class="nav-link" href="#system" data-toggle="tab">System</a>
         </li>
     </ul>
+    <!-- END MAIN MENU -->
+
+    <!-- APP CONTENTS -->
     <div class="card-body tab-content">
         <div id="workouts" class="tab-pane active">
-            <h2>Workouts</h2>
+            <?php include "workouts.php"; ?>
         </div>
         <div id="system" class="tab-pane">
             <h2>System</h2>
             <button id="refreshDataBase" type="button" class="btn btn-primary">Fetch New Data From darebee.com</button>
         </div>
     </div>
+    <!-- END APP CONTENTS -->
 </div>
