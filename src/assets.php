ff4c6ae5aad2aa0a157e61d9d7579ea5e919d008
leweke <male233@001NTB1066.ad.aconso.com>
2019-12-18 15:14:56 +0100

    add basic authentication (GET-based...) with sessions

--- src/assets.php
+++ src/assets.php
@@ -1,14 +1,22 @@
 <?php
 function authenticate() {
-    return true; //todo -> implement POST-authentication
-    $username = null;
-    if(!empty($_POST['username'])) {
-        $username = $_POST['username'];
+    if(isset($_SESSION['user_id'])) {
+        return True;
+    } else {
+        $username = null;
+        if(!empty($_GET['username'])) {
+            $username = $_GET['username'];
+        }
+        $password = null;
+        if(!empty($_GET['password'])) {
+            $password = $_GET['password'];
+        }
+        if(('marco' === $username) && ('Test1234' === $password)) {
+            $_SESSION['user_id'] = 'admin';
+            return True;
+        } else {
+            return False;
+        }
     }
-    $password = null;
-    if(!empty($_POST['password'])) {
-        $password = $_POST['password'];
-    }
-    return (('marco' === $username) && ('Test1234' === $password));
 }
 ?>
