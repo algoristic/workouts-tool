144cce1b5d68e22e101d976677d43ae22373c191
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-06 15:41:21 +0100

    'dynamic' loading of tabs

--- src/content/app.php
+++ src/content/app.php
@@ -1,27 +1,30 @@
+<?php
+$tabs = array(
+    'workouts' => 'Workouts',
+    'programs' => 'Programs',
+    'system' => 'System'
+);
+?>
 <div class="card">
     <!-- MAIN MENU -->
     <ul class="card-header nav nav-tabs" role="tablist">
-        <li class="nav-item">
-            <a class="nav-link <?php echo getActiveMarker('workouts'); ?>" href="#workouts">Workouts</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link  <?php echo getActiveMarker('system'); ?>" href="#system">System</a>
-        </li>
+        <?php foreach ($tabs as $tab => $name): ?>
+            <li class="nav-item">
+                <a class="nav-link <?php echo getActiveMarker($tab); ?>" href="#<?php echo $tab ?>"><?php echo $name ?></a>
+            </li>
+        <?php endforeach ?>
     </ul>
     <!-- END MAIN MENU -->
 
     <!-- APP CONTENTS -->
     <div class="card-body tab-content">
-        <div class="tab-pane <?php echo getActiveMarker('workouts'); ?>">
-            <?php if(isActivePage('workouts')): ?>
-                <?php include "workouts.php"; ?>
-            <?php endif ?>
-        </div>
-        <div class="tab-pane <?php echo getActiveMarker('system'); ?>">
-            <?php if(isActivePage('system')): ?>
-                <?php include "system.php"; ?>
-            <?php endif ?>
-        </div>
+        <?php foreach ($tabs as $tab => $name): ?>
+            <div class="tab-pane <?php echo getActiveMarker($tab); ?>">
+                <?php if(isActivePage($tab)): ?>
+                    <?php include ($tab . ".php"); ?>
+                <?php endif ?>
+            </div>
+        <?php endforeach ?>
     </div>
     <!-- END APP CONTENTS -->
 </div>
