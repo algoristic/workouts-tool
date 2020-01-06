7aa281d49f57c13463a514150057af433c56d32b
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-06 15:29:53 +0100

    make fetch api (programs) accessible through ui

--- src/content/system.php
+++ src/content/system.php
@@ -1,2 +1,4 @@
-<button id="refreshDataBase" type="button" class="btn btn-primary">Fetch New Data From darebee.com</button>
+<p>Fetch data from <a href="https://darebee.com/" target="_blank">darebee.com</a></p>
+<button id="fetchWorkouts" type="button" class="btn btn-block btn-primary">Fetch workouts</button>
+<button id="fetchPrograms" type="button" class="btn btn-block btn-primary">Fetch programs</button>
 <script src="/resources/js/system.js"></script>
--- src/resources/js/system.js
+++ src/resources/js/system.js
@@ -1,10 +1,19 @@
 $(function() {
-    $('#refreshDataBase').click(function() {
+    $('#fetchWorkouts').click(function() {
         load();
         $.ajax({
             url: 'https://workout.marco-leweke.de/api/fetch/workouts'
         }).done((response) => {
             alert('Fetched ' + response.created + ' new workouts (' + response.total + ' total)');
+            ready();
+        });
+    });
+    $('#fetchPrograms').click(function() {
+        load();
+        $.ajax({
+            url: 'https://workout.marco-leweke.de/api/fetch/programs'
+        }).done((response) => {
+            alert('Fetched ' + response.created + ' new programs (' + response.total + ' total)');
             ready();
         });
     });
