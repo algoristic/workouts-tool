ab566261a49625cfc5bf112d89334c801ff57e2f
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-06 08:54:38 +0100

    formatting

--- src/content/login.php
+++ src/content/login.php
@@ -3,12 +3,12 @@
         <h2>Login</h2>
     </div>
     <form action="index.php" method="get">
-    <div class="card-body">
-        <input class="form-control mb-2" type="text" name="username" placeholder="Username">
-        <input class="form-control" type="password" name="password" placeholder="Password">
-    </div>
-    <div class="card-footer">
-        <input class="btn btn-primary" type="submit" value="Submit">
-    </div>
+        <div class="card-body">
+            <input class="form-control mb-2" type="text" name="username" placeholder="Username">
+            <input class="form-control" type="password" name="password" placeholder="Password">
+        </div>
+        <div class="card-footer">
+            <input class="btn btn-primary" type="submit" value="Submit">
+        </div>
     </form>
 </div>
