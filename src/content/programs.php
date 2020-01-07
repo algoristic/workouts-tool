6560ba902dab1237b1d4f5fd80a590ba5358aaca
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-07 08:41:32 +0100

    simple program overview

--- src/content/programs.php
+++ src/content/programs.php
@@ -11,11 +11,30 @@
         <?php if($programs->num_rows > 0): ?>
             <?php foreach ($programs as $key => $program): ?>
                 <tr id="<?php echo $program['id'] ?>" program-description="<?php echo htmlspecialchars($program['description']) ?>">
-                    <td class="program-name"><strong><?php echo $program['name'] ?></strong></td>
+                    <td class="program-name" details-target="<?php echo $program['id'] ?>"><strong><?php echo $program['name'] ?></strong></td>
                     <td class="days-col"><?php echo $program['days'] ?></td>
                 </tr>
             <?php endforeach ?>
         <?php endif ?>
     </tbody>
 </table>
+<div id="info-modal" class="mobile-fullscreen workout-modal modal">
+    <div class="modal-dialog modal-lg modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img id="intro-img" class="img-fluid" src="" alt="">
+                    </div>
+                    <div class="col-md-6">
+                        <div id="program-description"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <script src="/resources/js/programs.js"></script>
--- src/resources/js/programs.js
+++ src/resources/js/programs.js
@@ -1,4 +1,35 @@
+function getProgramMediaLink(program) {
+    return './media/programs/' + program;
+}
+
+function getIntroLink(program) {
+    return (getProgramMediaLink(program) + '/intro.jpg');
+}
+
+function gatherProgramInfos(program) {
+    let info = {};
+    let row = $('#' + program);
+    info.description = row.attr('program-description');
+    info.name = row.find('.program-name strong').text();
+    //info.days = row.find('.days-col').text();
+    return info;
+}
+
+function openInfoModal(program) {
+    $('#intro-img').attr('src', '');
+    let introLink = getIntroLink(program);
+    $('#intro-img').attr('src', introLink);
+    let info = gatherProgramInfos(program);
+    $('#intro-img').attr('alt', info.name);
+    $('#program-description').html(htmlDecode(info.description));
+    $('#info-modal').modal();
+}
+
 $(function() {
+    $('#programs-table .program-name').click(function() {
+        let program = $(this).attr('details-target');
+        openInfoModal(program);
+    });
     $('#programs-table').DataTable({
         ordering: false,
         lengthChange: false,
