5b49a8054137fa399f4d2e1bdcbeccd2d30c6898
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-08 11:46:40 +0100

    training routine overview (draft)

---
+++ src/content/routine.php
@@ -0,0 +1,13 @@
+<table id="routines-table" class="table">
+    <thead>
+        <tr>
+            <th>Day</th>
+            <th>Name</th>
+        </tr>
+    </thead>
+    <tbody>
+
+    </tbody>
+</table>
+
+<script src="/resources/js/routine.js"></script>
---
+++ src/resources/js/routine.js
@@ -0,0 +1,12 @@
+$(function() {
+    var table = $('#routines-table').DataTable({
+        ordering: false,
+        lengthChange: false,
+        searching: false,
+        paging: false,
+        info: false,
+        language: {
+            emptyTable: 'No training routines yet'
+        }
+    });
+});
