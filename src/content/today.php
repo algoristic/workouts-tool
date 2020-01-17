4253347cbf3cc8aebccbdd5a756657495e8099dd
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-17 14:46:42 +0100

    new default page

--- src/content/today.php
+++ src/content/today.php
@@ -1,2 +1,13 @@
-
+<div class="tab-content">
+    <div context="pre-workout" class="tab-pane active">
+        <h2>Warmup</h2>
+    </div>
+    <div context="main-workout" class="tab-pane active">
+        Main Workout
+    </div>
+    <div context="post-workout" class="tab-pane active">
+        Cooldown
+    </div>
+</div>
+<button type="button" class="btn btn-primary"></button>
 <script src="/resources/js/today.js"></script>
