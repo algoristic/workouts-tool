68813b3a24fac1de1e36d8834ec99be189385be8
leweke <male233@001NTB1066.ad.aconso.com>
2020-01-23 08:11:59 +0100

    correct systems tab view (padding)

--- src/content/system.php
+++ src/content/system.php
@@ -1,4 +1,6 @@
-<p>Fetch data from <a href="https://darebee.com/" target="_blank">darebee.com</a></p>
-<button id="fetchWorkouts" type="button" class="btn btn-block btn-primary">Fetch workouts</button>
-<button id="fetchPrograms" type="button" class="btn btn-block btn-primary">Fetch programs</button>
-<script src="/resources/js/system.js"></script>
+<div class="px-2">
+    <p>Fetch data from <a href="https://darebee.com/" target="_blank">darebee.com</a></p>
+    <button id="fetchWorkouts" type="button" class="btn btn-block btn-primary">Fetch workouts</button>
+    <button id="fetchPrograms" type="button" class="btn btn-block btn-primary">Fetch programs</button>
+    <script src="/resources/js/system.js"></script>
+</div>
