5a5b3a2a8abdbfe8666d23a2943068cb82ca898c
Marco Leweke <mail@marco-leweke.de>
2020-11-10 13:33:12 +0100

    Update logout.php

--- src/logout.php
+++ src/logout.php
@@ -7,9 +7,9 @@ session_destroy();
 <html lang="en">
     <head>
         <title>Logout</title>
-        <meta http-equiv="refresh" content="0; url=http://workout.marco-leweke.de/" />
+        <meta http-equiv="refresh" content="0; url=http://workout.algoristic.de/" />
     </head>
     <body>
-        <p>This page redirects you automatically to the login. If you do not get redirected, click <a href="http://workout.marco-leweke.de/">here</a>!</p>
+        <p>This page redirects you automatically to the login. If you do not get redirected, click <a href="http://workout.algoristic.de/">here</a>!</p>
     </body>
 </html>
