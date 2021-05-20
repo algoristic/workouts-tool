8071511fa406aaa9c11c1dfcc2b1db00813c3f60
leweke <male233@001NTB1066.ad.aconso.com>
2021-05-20 08:22:55 +0200

    prefill username with url-parameter

--- src/content/login.php
+++ src/content/login.php
@@ -4,7 +4,13 @@
     </div>
     <form action="index.php" method="post">
         <div class="card-body">
-            <input class="form-control mb-2" type="text" name="username" placeholder="Username">
+            <?php $username = $_GET['username']; ?>
+            <?php $usernameExists = ($username !== NULL) ?>
+            <input class="form-control mb-2" type="text" name="username" placeholder="Username"
+            <?php if($usernameExists): ?>
+                value="<?php echo $username ?>"
+            <?php endif ?>
+            >
             <input class="form-control" type="password" name="password" placeholder="Password">
         </div>
         <div class="card-footer">
