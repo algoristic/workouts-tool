513545abd0dfe130eb6f1d261ca27720767e6e5a
leweke <male233@001NTB1066.ad.aconso.com>
2021-05-20 08:22:41 +0200

    Correct domain (https instead of http everywhere)

--- src/logout.php
+++ src/logout.php
@@ -7,9 +7,9 @@ session_destroy();
 <html lang="en">
     <head>
         <title>Logout</title>
-        <meta http-equiv="refresh" content="0; url=http://workout.algoristic.de/" />
+        <meta http-equiv="refresh" content="0; url=https://workout.algoristic.de/" />
     </head>
     <body>
-        <p>This page redirects you automatically to the login. If you do not get redirected, click <a href="http://workout.algoristic.de/">here</a>!</p>
+        <p>This page redirects you automatically to the login. If you do not get redirected, click <a href="https://workout.algoristic.de/">here</a>!</p>
     </body>
 </html>
